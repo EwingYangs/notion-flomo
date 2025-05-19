edf5b7bd9eac92ced97a494eaa45dfbe891f160f
ruihe <ruihe>
2025-05-19 23:13:54 +0800

    bugfix

--- flomo2notion.py
+++ flomo2notion.py
@@ -324,7 +324,7 @@ class Flomo2Notion:
             if memo['slug'] in slug_map.keys():
                 # 是否全量更新，默认否
                 full_update = os.getenv("FULL_UPDATE", False)
-                interval_day = os.getenv("UPDATE_INTERVAL_DAY", 7)
+                interval_day = os.getenv("UPDATE_INTERVAL_DAY", 7000)
                 if not full_update and not is_within_n_days(memo['updated_at'], interval_day):
                     logger.info(f"{progress} ⏭️ 跳过记录 [slug: {memo['slug']}] - 更新时间超过 {interval_day} 天")
                     self.skip_count += 1
